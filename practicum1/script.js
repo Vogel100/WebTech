cf6c28ffafbd74320f545ef09e9f8ea535bc048b
Vogel100 <fabbimaggi@hotmail.com>
2016-02-24 16:26:53 +0100

    A bit shorter

--- practicum1/script.js
+++ practicum1/script.js
@@ -5,7 +5,6 @@ $(function() {
 		'<li><a href="mechanics.html">Mechanics</a></li>' +
 		'<li><a href="history.html">History</a></li>' +
 		'<li><a href="support.html">Browser support</a></li>' +
-		'<li><a href="methods.html">Different methods</a></li>' +
-		'</ul>'
+		'<li><a href="methods.html">Different methods</a></li></ul>'
 	);
 });
