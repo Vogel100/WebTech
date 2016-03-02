1cb76574b50b4a98bbdb2d0ad978b2083c4c644a
Vogel100 <fabbimaggi@hotmail.com>
2016-03-02 16:59:52 +0100

    Adding a references list (not finished yet)

--- practicum1/script.js
+++ practicum1/script.js
@@ -7,4 +7,22 @@ $(function() {
 		'<li><a href="support.html">Browser support</a></li>' +
 		'<li><a href="coding.html">Coding</a></li></ul>'
 	);
+	/*UNFINISHED
+	var $refList = $('<ol>');
+	var $refSection = $('<section>', {'class': 'reflist', html: '<h2>References</h2>'}).append($refList);
+	var refCount = 0;
+	$('a').each(function() {
+		var $link = $(this);
+		var url = $link.attr('href');
+		if (/^https?:\/\//.test(url)) {
+			refCount++;
+			$link.attr('id', 'ref' + refCount);
+			$backlink = $('<a>', {'class': 'backlink', href: '#ref' +refCount, text:'^'}).click()
+			$refList.append('<li><a class="backlink" href="#ref' + refCount + 
+				'">^</a> <a class="reflink" href="' + url + '">' + url +'</a></li>');
+		}
+	});
+	if (refCount > 0)
+		$('article').append($refSection);
+	*/
 });
